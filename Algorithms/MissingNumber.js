2b989236af3c9f17d3e45850be2ddda2b7e7f0fc
Doug Shamoo <doug.shamoo@gmail.com>
2016-01-23 14:06:16 -0800

    Implement Missing Number JavaScript solution

---
+++ Algorithms/MissingNumber.js
@@ -0,0 +1,30 @@
+/*
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+For example,
+Given nums = [0, 1, 3] return 2.
+
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+  var targetSum = 0;
+  var sum = 0;
+
+  // handles array of [0] or [1]
+  if (nums.length === 1) return 1 - nums[0];
+
+  // calculate sum and targetSum
+  for (var i = 0; i < nums.length; i++) {
+    targetSum += i;
+    sum += nums[i];
+  }
+  targetSum += nums.length;
+
+  return targetSum - sum;
+};
